6976d74dc0207f248d298b904f270f3bd7fecbae
Omar Lachhab <30928791+omarlachhab@users.noreply.github.com>
2023-03-13 12:16:26 +0100

    new

--- components/btn/RoundedButton.tsx
+++ components/btn/RoundedButton.tsx
@@ -8,6 +8,7 @@ type Props = {
 function roundedButton({ children, onClick }: Props) {
   return (
     <button
+      aria-label=""
       onClick={onClick}
       className="flex h-12 w-12 items-center justify-center rounded-full shadow-md hover:bg-slate-200"
     >
